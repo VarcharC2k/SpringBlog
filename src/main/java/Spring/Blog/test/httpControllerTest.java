16733138600f7e3d549e9e8d37ed7af05b2b4d60
KGCho <rudrms950@naver.com>
2023-04-15 14:46:03 +0900

    http Controller text 완료

---
+++ src/main/java/Spring/Blog/test/httpControllerTest.java
@@ -0,0 +1,38 @@
+package Spring.Blog.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+//RestController >> 사용자가 요청한 항목에 대해 Data를 응답해줌
+//Controller >> 사용자의 요청에 HTML 파일로 응답해줌
+
+@RestController
+public class httpControllerTest {
+
+    //인터넷 브라우저 요청은 무조건 get 요청을 할 수 밖에 없다
+    //http://localhost:8080/http/get
+    @GetMapping("/http/get")
+    public String getTest(Member m) {
+        return "get 요청 : "+ m.getId() + "," + m.getUsername() + "," + m.getPassword() + "," + m.getEmail();
+    }
+
+    //http://localhost:8080/http/post
+    @PostMapping("/http/post")
+    public String postTest(@RequestBody Member m) {
+        return "post 요청 : "+ m.getId() + "," + m.getUsername() + "," + m.getPassword() + "," + m.getEmail();
+//        return "post 요청 : " + text;
+    }
+
+    //http://localhost:8080/http/put
+    @PutMapping("/http/put")
+    public String putTest(@RequestBody Member m) {
+        return "put 요청 : " + m.getId() + "," + m.getUsername() + "," + m.getPassword() + "," + m.getEmail();
+    }
+
+    //http://localhost:8080/http/delete
+    @DeleteMapping("/http/delete")
+    public String deleteTest() {
+        return "delete 요청";
+    }
+
+}
