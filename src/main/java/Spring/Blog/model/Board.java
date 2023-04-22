413918ff76c90f1a48722ca51950b3445f8c5b85
KGCho <rudrms950@naver.com>
2023-04-22 10:45:39 +0900

    Board Reply 설정 및 Fetch 전략 설정

--- src/main/java/Spring/Blog/model/Board.java
+++ src/main/java/Spring/Blog/model/Board.java
@@ -9,6 +9,7 @@ import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -30,9 +31,17 @@ public class Board {
     @ColumnDefault("0")
     private int count; //조회수
 
-    @ManyToOne //1:* 관계, Many는 Board, One은 유저
+
+    //FetchType.Eager 과 Lazy 의 차이
+    //Eager : 즉시 반환, 로딩 시 필요한 데이터를 즉시 반활 할 때 사용
+    //Lazy : 지연 반환, 요청에 따른 데이터 처리시 지연시켜 반환
+    @ManyToOne(fetch = FetchType.EAGER) //1:* 관계, Many는 Board, One은 유저
+    //fetch = FetchType.EAGER 가져오는 것이 단일일때 사용
     @JoinColumn(name="userId") // 실제 DB 생성시 컬럼명은 userId
     private User user; //작성자, DB는 오브젝트를 저장할수 없기 때문에 FK를 사용
+
+    @OneToMany(mappedBy = "board",fetch = FetchType.EAGER) //mappedBy : 연관관계의 주인이 아니다 (FK가 아님), 즉 DB에 컬럼 생성을 하지 않음
+    private List<Reply> reply;
 
     @CreationTimestamp
     private Timestamp createDate;
