9024c746d3369ea7c7b51d4607e7248c75f52dfe
KGCho <rudrms950@naver.com>
2023-05-01 11:49:54 +0900

    Exception Handler 구성

---
+++ src/main/java/Spring/Blog/handler/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package Spring.Blog.handler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.ExecutionException;
+
+@ControllerAdvice //모든 Exception이 발생한 경우 해당 클래스로 들어오게 됨
+@RestController
+public class GlobalExceptionHandler {
+
+//    특정한 Exception만 받는 경우
+//    @ExceptionHandler(value = IllegalArgumentException.class) //IllegalArgumentException이 들어오는 경우 해당 동작 수행
+//    public String handleArgumentException(IllegalArgumentException e) {
+//        return "<h1>" + e.getMessage() + "</h1>";
+//    }
+
+    @ExceptionHandler(value = Exception.class)
+    public String allException(Exception e) {
+        return "<h2>" + e.getMessage() + "</h2>";
+    }
+}
