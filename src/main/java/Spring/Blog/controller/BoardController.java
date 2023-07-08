c0337bc4c85dbf4b29e580bd846f39d16f1caedf
DeLancer <rudrms950@gmail.com>
2023-07-08 14:28:20 +0900

    로그인 로직 구현 중

--- src/main/java/Spring/Blog/controller/BoardController.java
+++ src/main/java/Spring/Blog/controller/BoardController.java
@@ -6,14 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class BoardController {
 
-    @GetMapping("/")
+    @GetMapping({"","/"})
     public String index(){
         return "index";
     }
 
-    @GetMapping("/home")
-    public String goHome() {
-        return "content/home";
-    }
+//    @GetMapping("/home")
+//    public String goHome() {
+//        return "content/home";
+//    }
 
 }
